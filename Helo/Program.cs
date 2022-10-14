236cae2e0add2104c12a8e337557a35acd8e044d
FORHAD HOSSAIN <forhadhossaincub@gmail.com>
2022-10-14 20:45:16 +0600

    Hellow World ! Test Program

--- Helo/Program.cs
+++ Helo/Program.cs
@@ -5,23 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Helo
+namespace Hello
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Calculator");
-
-            int a=10;
-            int b=30;
-            Console.WriteLine(a + b);
-
-            string name = "Arif Hossain";
-            string uName = "Canadian University of Bangladesh";
-            decimal salary = 500000M;
-
-
+            Console.Write("Hellow World !");
 
             Console.ReadKey();
             
